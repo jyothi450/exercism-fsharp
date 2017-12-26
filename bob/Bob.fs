3d7b8f7349230a1c21941fc6278b7696a9c5b127
Jyothi Thondepu <jyothi.thondepu@jet.com>
2017-12-25 21:00:18 -0500

    next set of exercises

---
+++ bob/Bob.fs
@@ -0,0 +1,16 @@
+﻿module Bob
+
+let response (input: string): string =
+    let isQuestion (x : string) = x.EndsWith('?')
+    let isYell (x:string) = Seq.exists System.Char.IsLetter x && x = x.ToUpper()
+    let isForcefulQuestion (x:string) = isQuestion(x) && isYell(x)
+    let trimmedInput = input.Trim()
+
+    match trimmedInput with
+        | _ when trimmedInput |> System.String.IsNullOrWhiteSpace -> "Fine. Be that way!"
+        | _ when trimmedInput |> isForcefulQuestion -> "Calm down, I know what I'm doing!"
+        | _ when trimmedInput |> isQuestion -> "Sure."
+        | _ when trimmedInput |> isYell -> "Whoa, chill out!"
+        | _ -> "Whatever."
+
+        
